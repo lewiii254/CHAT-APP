72d321e8fa8521c73ea882774820d4e81230639c
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-03-19 18:45:47 +0400

    final push of the day

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Logo from "../assets/logo.png";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div>
       <div className="bg-white rounded-full">
-        <img src={Logo} alt="Logo" />
+        <img src={Logo} alt="Logo" className='size-10 object-contain'/>
       </div>
-      <div>sideroutes</div>
+      <div>
+        <link></link>
+      </div>
       <div>user</div>
     </div>
   );
